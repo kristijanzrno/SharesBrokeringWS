c8c4f7b78303aaf363194c0f8a5497e02b527edf
kristijanzrno <superkiki.info.android@gmail.com>
2020-01-07 04:14:07 +0100

    Configured the schema file, generated classes

--- src/java/com/shares/brokering/BrokeringWS.java
+++ src/java/com/shares/brokering/BrokeringWS.java
@@ -5,6 +5,7 @@
  */
 package com.shares.brokering;
 
+import java.util.ArrayList;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -24,5 +25,31 @@ public class BrokeringWS {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+    
+    @WebMethod(operationName = "test")
+    public String test(){
+        Data.Shares shares = new Data.Shares();
+        Data.Share share = new Data.Share();
+        share.setCompanyName("company");
+        share.setCompanySymbol("CMP");
+        share.setNumberOfShares(3244);
+        Data.Price price = new Data.Price();
+        price.setCurrency("BAM");
+        price.setValue(23.23);
+        share.setPrice(price);
+        shares.getShares().add(share);
+        shares.getShares().add(share);
+        try {            
+            javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(price.getClass().getPackage().getName());
+            javax.xml.bind.Marshaller marshaller = jaxbCtx.createMarshaller();
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8"); //NOI18N
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.marshal(shares, System.out);
+        } catch (javax.xml.bind.JAXBException ex) {
+            // XXXTODO Handle exception
+            java.util.logging.Logger.getLogger("global").log(java.util.logging.Level.SEVERE, null, ex); //NOI18N
+        }
+        return "";
     }
 }
